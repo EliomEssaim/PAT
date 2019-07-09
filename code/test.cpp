b2afa54d2b97dc4dd7c2a288538c34cc92b07399
dragon jack <31036659+EliomEssaim@users.noreply.github.com>
2019-07-09 10:03:09 +0800

    update

--- code/test.cpp
+++ code/test.cpp
@@ -2,13 +2,34 @@
 #include<cctype>
 #include<string>
 #include<math.h>
+#include<vector>
+#include<algorithm>
 using namespace std;
-int main()
-{
-    double p,r;
-    int depth=2;
-    p=1.80;
-    r=1.00;
-    printf("%.5f\n",7.0*p*pow(1+r/100,depth));
+struct node {
+    int x, y;
+};
+bool cmp1(node a, node b) {
+    return a.x < b.x;
+}
+int main() {
+    vector<int> v(3);
+    v={2,3,7};
+    vector<string> v2;
+    v2.push_back("hello");
+    v2.push_back("world");
+    v2.push_back("kksk");
+    string sum = accumulate(v2.begin() , v2.end() , string("µþÖ½"));
+    cout<<sum<<endl;
+    int arr[4]={7,5,3,6};
+    vector<node> v1(3);
+    v1={node{7,4},{6,3}};
+    cout << *max_element(v.begin(), v.end())<<endl;
+    cout << *min_element(arr, arr + 4)<<endl;
+    cout << (*max_element(v1.begin(), v1.end(), cmp1)).y<<endl;
+
+
+    char* str2=to_string(555);;
+
+    cout<<str2<<endl;
     return 0;
 }
